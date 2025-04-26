7a39e4db919f630a634887582a8f279452796e38
aysha3f <tahiaysha220@gmail.com>
2025-04-26 23:13:27 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,25 @@
+const nayok = {
+name: "Sakib Khan",
+id: 121,
+address: 'novel cinema',
+isSingle: true,
+friends:['Apu', 'Raaz', 'Salam', "Amin"],
+act: function (){
+    console.log('Acting like a nayok');
+} ,
+movies:[{name: 'no 1', year: 2015},{name: 'king khan', year: 2018}],
+car: {
+    brand: 'teasla',
+    price: 50000000,
+    made: 2025,
+    manufacturer: {
+name: 'tesla',
+ceo: "Elon Mask",
+country : "USA",
+    }
+}
+}
+// console.log(student.car);
+// console.log(nayok.act);
+nayok.act()
+//node object.js
