342cf26ff757e35122dd8dab9589fe96629ab874
aysha3f <tahiaysha220@gmail.com>
2025-04-30 23:36:23 +0600

    using for of loop to get the array

--- match.js
+++ match.js
@@ -1,7 +1,11 @@
 // node match.js
 
 const numbers = [45, 65, 23, 98, 19];
-for(let i = 0; i < numbers.length; i++){
-    const number = numbers[i];
-    console.log(number);
-}
+// for(let i = 0; i < numbers.length; i++){
+//     const number = numbers[i];
+//     console.log(number);
+// }
+
+// for(const number of numbers){
+//     console.log(number)
+// }
